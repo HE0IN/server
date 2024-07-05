ea10d0f2e96983f0bd98f134a08855d31b6de27a
HE0IN <syy1839@naver.com>
2024-07-05 16:55:48 +0900

    20240705 정리

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, function() {
+app.listen(8080, function () {
     console.log('8080 포트에서 서버 실행 중');
 });
+
+let text="집에가기 30분전"
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 app.get('/todos', (req, res) => {
-    res.send('<h1>할 일 목록입니다.</h1>');
+    res.send(`${text}`);
 });
---
+++ template.js
@@ -0,0 +1,5 @@
+let text = "배문형";
+let text2 = '배문\n형';
+let text3 = '배문형';
+
+console.log(text, text2, text3)
